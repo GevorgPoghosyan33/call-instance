55c9fcd22d9c289c5badd7dd2a1b208f07bd01ec
Gevorg <gpogh2000@gmail.com>
2021-06-09 14:22:36 -0700

    added added call magic function

--- call-method.py
+++ call-method.py
@@ -1,1 +1,12 @@
-print("1st version")
+class CallIns:
+    def __init__(self):
+        self.sum = 0
+
+    def __call__(self, *args):
+        for i in args:
+            self.sum += i
+        return f'summary is: {self.sum}'
+
+
+summary1 = CallIns()
+print(summary1(5, 7, 8))
